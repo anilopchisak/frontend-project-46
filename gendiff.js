eae7313c61acee9d8b48cf40cc677bee91b9803c
Polina Kasich <poli.key@yandex.ru>
2025-04-27 17:04:19 +0500

    gendiff.js: add genDiff()

--- gendiff.js
+++ gendiff.js
@@ -1,7 +1,31 @@
 import { Command } from "commander";
-import process from 'node:process';
 import path from 'node:path';
-import handleParse from "./handleParse.js";
+import handleParse from "./src/handleParse.js";
+import _ from 'lodash';
+
+const genDiff = ([data1, data2]) => {
+    const result = {};
+    const keys1 = Object.keys(data1);
+    const keys2 = Object.keys(data2);
+    const keys = _.union(keys1, keys2);
+    keys.forEach((key) => {
+        if (!Object.hasOwn(data1, key)) {
+            result[`+ ${key}`] = data2[key];
+        }
+        else if (!Object.hasOwn(data2, key)) {
+            result[`- ${key}`] = data1[key];
+        }
+        else if (data1[key] !== data2[key]) {
+            result[`- ${key}`] = data1[key];
+            result[`+ ${key}`] = data2[key];
+        }
+        else {
+            result[key] = data2[key];
+        }
+    });
+    console.log(JSON.stringify(result));
+    return JSON.stringify(result);
+}
 
 const program = new Command();
 program
@@ -13,7 +37,8 @@ program
     .action((filepath1, filepath2) => {
         const newPath1 = path.resolve(filepath1);
         const newPath2 = path.resolve(filepath2);
-        handleParse([newPath1, newPath2]);
+        const parsed = handleParse([newPath1, newPath2]);
+        const diff = genDiff(parsed);
     });
 
 program.parse();
---
+++ src/handleParse.js
@@ -0,0 +1,17 @@
+import * as fs from 'node:fs';
+
+export default (paths) => {
+    const parsed = paths
+        .filter((path) => path.split('.')[1] === 'json')
+        .map((path) => 
+            fs.readFileSync(path, (err, data) => {
+                if (err) throw err;
+                console.log(data);
+            }))
+        .map((file) => {
+            const parsedFile = JSON.parse(file)
+            console.log(parsedFile);
+            return parsedFile;
+        });
+    return parsed;
+}
