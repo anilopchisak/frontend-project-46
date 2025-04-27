6b03f666bee162cbaf099223574c8ff91d4f2b4d
Polina Kasich <poli.key@yandex.ru>
2025-04-27 14:53:59 +0500

    gendiff.js: add arguments, option format

--- gendiff.js
+++ gendiff.js
@@ -3,6 +3,9 @@ import { Command } from "commander";
 const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('0.1.0');
+    .version('0.1.0')
+    .argument('filepath1')
+    .argument('filepath2')
+    .option('-f, --format [type]', 'output format');
 
 program.parse();
