9fd143666f541cd5091c7d4be400d03b13d110bb
Polina Kasich <poli.key@yandex.ru>
2025-04-30 11:48:35 +0500

    fix genDiff.js: result

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,25 +1,26 @@
 import _ from 'lodash'
 
 export default ([data1, data2]) => {
-  const result = {}
+  let result = '{\n'
   const keys1 = Object.keys(data1)
   const keys2 = Object.keys(data2)
   const keys = _.union(keys1, keys2).sort()
   keys.forEach((key) => {
     if (!Object.hasOwn(data1, key)) {
-      result[`+ ${key}`] = data2[key]
+      result += `  + ${key} = ${data2[key]}\n`
     }
     else if (!Object.hasOwn(data2, key)) {
-      result[`- ${key}`] = data1[key]
+      result += `  - ${key} = ${data1[key]}\n`
     }
     else if (data1[key] !== data2[key]) {
-      result[`- ${key}`] = data1[key]
-      result[`+ ${key}`] = data2[key]
+      result += `  - ${key} = ${data1[key]}\n`
+      result += `  + ${key} = ${data2[key]}\n`
     }
     else {
-      result[key] = data2[key]
+      result += `    ${key} = ${data2[key]}\n`
     }
   })
+  result += `}`
   console.log(result)
-  return JSON.stringify(result)
+  return result
 }
