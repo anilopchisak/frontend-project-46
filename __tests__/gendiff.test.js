3470dc6e088474245afb4eecba15441e8575901f
Polina Kasich <poli.key@yandex.ru>
2025-04-30 19:47:40 +0500

    fix fixtures and tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,18 +2,18 @@ import handleParse from '../src/handleParse.js'
 import genDiff from '../src/genDiff.js'
 import { readFile } from '../src/readFile.js'
 
+const expected = readFile('expected.txt')
+
 describe('genDiff', () => {
-  test('Flat JSON files comparison', () => {
+  test('json file', () => {
     const parsed = handleParse(['file1.json', 'file2.json'])
     const diff = genDiff(parsed)
-    const expected = readFile('expected.txt')
     expect(diff).toEqual(expected)
   })
 
-  test('Flat yaml files comparison', () => {
+  test('yaml/yml file', () => {
     const parsed = handleParse(['file1.yml', 'file2.yaml'])
     const diff = genDiff(parsed)
-    const expected = readFile('expected.txt')
     expect(diff).toEqual(expected)
   })
 })
