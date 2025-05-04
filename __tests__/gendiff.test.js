c8ae395bf9c8bd50d99d9f786a1625722e86d481
Polina Kasich <poli.key@yandex.ru>
2025-05-04 14:41:01 +0500

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,15 +11,15 @@ describe('genDiff', () => {
     expect(diff).toEqual(expected)
   })
 
-  // test('yaml/yml files', () => {
-  //   const parsed = handleParse(['file1.yml', 'file2.yaml'])
-  //   const diff = genDiff(parsed)
-  //   expect(diff).toEqual(expected)
-  // })
+  test('yaml/yml files', () => {
+    const parsed = handleParse(['file1.yml', 'file2.yaml'])
+    const diff = genDiff(parsed)
+    expect(diff).toEqual(expected)
+  })
 
-  // test('yaml and json files', () => {
-  //   const parsed = handleParse(['file1.yml', 'file2.json'])
-  //   const diff = genDiff(parsed)
-  //   expect(diff).toEqual(expected)
-  // })
+  test('yaml and json files', () => {
+    const parsed = handleParse(['file1.yml', 'file2.json'])
+    const diff = genDiff(parsed)
+    expect(diff).toEqual(expected)
+  })
 })
