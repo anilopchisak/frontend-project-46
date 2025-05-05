afed17ab0bdc2fa39d725554ef4e5a8c2bf1729f
Polina Kasich <poli.key@yandex.ru>
2025-05-05 15:06:47 +0500

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,8 @@ const program = new Command()
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .argument('filepath1')
-  .argument('filepath2')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
     const diff = interfaceGenDiff(filepath1, filepath2, options.format)
