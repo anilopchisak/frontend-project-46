74cb012ed48f3bc818ee4d9b87fab1c9e4684699
Polina Kasich <poli.key@yandex.ru>
2025-05-06 23:25:17 +0500

    fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
 import { fileURLToPath } from 'url'
-import interfaceGenDiff from '../src/interfaceGenDiff.js'
+import genDiff from '../src/interfaceGenDiff.js'
 
 const program = new Command()
-program
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2, options) => {
-    const diff = interfaceGenDiff(filepath1, filepath2, options.format)
-    console.log(diff)
-  })
+export const createCli = () => {
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.1.0')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format', 'stylish')
+    .action((filepath1, filepath2, options) => {
+      const diff = genDiff(filepath1, filepath2, options.format)
+      console.log(diff)
+    })
+}
 
 if (fileURLToPath(import.meta.url) === process.argv[1]) {
-  program.parse()
+  createCli().parse()
 }
--- src/interfaceGenDiff.js
+++ src/interfaceGenDiff.js
@@ -2,11 +2,9 @@ import handleParse from './parsers/handleParse.js'
 import genDiff from './diff/genDiff.js'
 import iFormatter from './formatters/index.js'
 
-const interfaceGenDiff = (filepath1, filepath2, formatName = 'stylish') => {
+export default (filepath1, filepath2, formatName = 'stylish') => {
   const parsed = handleParse([filepath1, filepath2])
   const diff = (genDiff(parsed))
   const formatter = iFormatter(formatName)
   return formatter(diff)
 }
-
-export default interfaceGenDiff
