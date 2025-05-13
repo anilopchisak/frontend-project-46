aae89b71e91f71b80edd2ae32b0c58c1671824b7
Polina Kasich <poli.key@yandex.ru>
2025-05-13 16:50:51 +0500

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,29 +1,40 @@
+import { fileURLToPath } from 'node:url';
 import path from 'node:path'
 import genDiff from '../src/genDiff.js'
 import { readFile } from '../src/readFile.js'
 
-const expectedStylish = readFile('expected_stylish.txt', '__fixtures__')
-const expectedPlain = readFile('expected_plain.txt', '__fixtures__')
-const expectedJson = JSON.stringify(JSON.parse(readFile('expected_json.json', '__fixtures__')))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const cases = [
+const fixturesPath = path.join(__dirname, '..', '__fixtures__')
+
+const expectedStylish = readFile('expected_stylish.txt', fixturesPath)
+const expectedPlain = readFile('expected_plain.txt', fixturesPath)
+const expectedJson = JSON.stringify(JSON.parse(readFile('expected_json.json', fixturesPath)))
+
+const pairs = [
   ['file1.json', 'file2.json'],
   ['file1.yml', 'file2.yaml'],
   ['file1.yml', 'file2.json'],
   ['file1.json', 'file2.yaml'],
 ]
 
+const cases = pairs.map(([file1, file2]) => [
+  path.join(fixturesPath, file1),
+  path.join(fixturesPath, file2),
+])
+
 describe.each(cases)('genDiff', (filename1, filename2) => {
   test('stylish', () => {
-    const diff = genDiff(path.join('__fixtures__', filename1), path.join('__fixtures__', filename2))
+    const diff = genDiff(filename1, filename2)
     expect(diff).toEqual(expectedStylish)
   })
   test('plain', () => {
-    const diff = genDiff(path.join('__fixtures__', filename1), path.join('__fixtures__', filename2), 'plain')
+    const diff = genDiff(filename1, filename2, 'plain')
     expect(diff).toEqual(expectedPlain)
   })
   test('json', () => {
-    const diff = genDiff(path.join('__fixtures__', filename1), path.join('__fixtures__', filename2), 'json')
+    const diff = genDiff(filename1, filename2, 'json')
     expect(diff).toEqual(expectedJson)
   })
 })
